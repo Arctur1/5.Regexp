9315711c37e41f396d4457fe2d3797b1a5f738ec
“AlexeyRozhnov” <rozhnov.alexey@gmail.com>
2021-06-06 20:32:25 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import re
+import csv
+import pandas
+words_pattern = '[а-яА-Я]+'
+phone_number = '(\+7|8)[\s(]*(\d{3})[)\s-]*(\d{3})[\s-]?(\d{2})[\s-]?(\d{2})'
+phone_replace = '\\1(\\2)\\3-\\4-\\5'
+
+with open("phonebook_raw.csv", encoding='utf-8') as f:
+    rows = csv.reader(f, delimiter=",")
+    contacts_list = list(rows)
+    for contact in contacts_list[1:]:
+        contact[:3] = [','.join(contact[:3])]
+    i = 1
+    while i < len(contacts_list):
+        name = ['', '', '']
+        pattern = re.findall(words_pattern, contacts_list[i][0], flags=re.IGNORECASE)
+        name[:len(pattern)] = pattern
+        del contacts_list[i][0]
+        contacts_list[i] = name + contacts_list[i]
+        contacts_list[i][-2] = contacts_list[i][-2].replace('(', '').replace(')', '')
+        contacts_list[i][-2] = re.sub(phone_number, phone_replace, contacts_list[i][-2])
+        contacts_list[i] = contacts_list[i][:7]
+        i += 1
+        df = pandas.DataFrame(contacts_list[1:], columns=contacts_list[0])
+        df.groupby('lastname').first().reset_index().to_csv(r'phonebook.csv', header=1, index=None, sep=',',
+                                                            mode='w', encoding='cp1251')
+
+
+
+
+
+
